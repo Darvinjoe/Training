0ff816b45a260708bd7057ae9b08986226a76238
Darvinjoe <darvin_joseph@kctechservices.com>
2021-09-03 13:11:59 +0530

    wishes

--- my_project_1/gcit 1.py
+++ my_project_1/gcit 1.py
@@ -1,19 +1,19 @@
 print("hello world")
-# name=input("what is your name?")
-# greetings = "hope you done good "+name
-# print(greetings)
+name=input("what is your name?")
+greetings = "hope you done good "+name
+print(greetings)
 
-# nums = [3, 4, 6, 2, 7]
-# nums.sort()
-# print(nums[-2])
+nums = [3, 4, 6, 2, 7]
+nums.sort()
+print(nums[-2])
 
-# nums=[1,2,3,4,4,9,7,4]
-# x=int(input("enter the number:"))
-# sum=0
-# for i in nums:
-#     if x==i:
-#         sum=sum+i
-# print(sum)
+nums=[1,2,3,4,4,9,7,4]
+x=int(input("enter the number:"))
+sum=0
+for i in nums:
+    if x==i:
+        sum=sum+i
+print(sum)
 
 
 x=10
