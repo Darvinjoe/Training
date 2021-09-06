b1f7e5f62452ea41cc66396eb44742f3f4994af7
Darvinjoe <darvin_joseph@kctechservices.com>
2021-09-06 21:53:02 +0530

    escape_sequence

---
+++ my_project_1/d(6.9.21)/sequence.py
@@ -0,0 +1,8 @@
+a="hi\nhow are you?\nwhat are you doing?\n"
+print(a)
+b="earth is an incridible place,\tteeming with animals,\tinsects,\tfish,\tbirds,\t plants and people.\nlike a noah\"s ark in space.\n"
+print(b)
+c='i won\'t drink pepsi'
+print(c)
+
+
