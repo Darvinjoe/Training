d8beffb43caac6e73a3d71f4717139290474f691
Darvinjoe <darvin_joseph@kctechservices.com>
2021-09-07 11:02:58 +0530

    json sequence

---
+++ my_project_1/d(6.9.21)/json file escape sequence.py
@@ -0,0 +1,10 @@
+a={
+"null": "null",
+"bool": True,
+"number": -2.3,
+"string": "i love bandung",
+"string\nwith escapes": "i\nlove\nbandung\\",
+"array": [1, 2, "tiga"],
+"object": {"foo": 1, "bar": "baz", "qux": []}
+}
+print(a)
